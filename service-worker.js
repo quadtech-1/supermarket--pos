73275cc512848645ae5077a222d43183cdd0f99d
quadtech-1 <oliviateam717@gmail.com>
2026-08-12 11:10:28 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+{
+  "name": "My Store",
+  "short_name": "My Store",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#f6f7f8",
+  "theme_color": "#171717",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "./icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "./icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
